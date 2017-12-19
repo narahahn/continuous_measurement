e662167541a85d1249dae585259176dd28d25cad
narahahn <n.hahn343@gmail.com>
2017-12-19 17:50:25 +0100

    Sound field reconstruction using continuous measurement for mono-frequency

--- example/discrete_elevations.py
+++ example/discrete_elevations.py
@@ -20,15 +20,15 @@ c = 343
 fs = 8000
 
 # Source
-xs = [0, 8, 0]
+xs = [0, 3, 0]
 source_type = 'point'
 
 # Excitation
-N = 400  # excitation period
+N = 800  # excitation period
 p = perfect_sequence_randomphase(N)
 
 # Sampling scheme
-max_sht_order = 20
+max_sht_order = 15
 x, weights = np.polynomial.legendre.leggauss(max_sht_order+1)
 weights *= np.pi / (max_sht_order+1)
 theta = np.arccos(x)
@@ -37,7 +37,7 @@ Q = len(theta)
 M = 2*max_sht_order + 2
 
 # Microphones
-R = 0.2
+R = 0.5
 Omega = 2*np.pi*fs / (N*M)
 L = int(2*np.pi/Omega*fs)
 t = (1/fs) * np.arange(L)
@@ -47,10 +47,6 @@ positions = np.stack((
         R*np.sin(theta[:, np.newaxis])*np.cos(phi), \
         R*np.sin(theta[:, np.newaxis])*np.sin(phi), \
         R*np.cos(theta[:, np.newaxis])*np.ones_like(phi)), axis=-1)
-
-# Experimental parameters
-#Omega_al = c / N / R  # anti-aliasing angular speed
-
 
 # Captured signal and additive noise
 snr = -np.infty
@@ -65,9 +61,10 @@ for q in range(Q):
 _, _, weights = micarray.modal.angular.grid_gauss(max_sht_order)
 azi = np.linspace(0, 2*np.pi, 2*max_sht_order+2, endpoint=False)
 x, weights = np.polynomial.legendre.leggauss(max_sht_order+1)
+weights *= np.pi / (max_sht_order+1)  # sum(weights)*(2*max_sht_order+2) = 4pi
 elev = np.arccos(x)
 
-
+# Continuous measurement
 h = np.zeros((Q, M, N))
 max_cht_order = (int(L/N)-1) // 2
 Nf = N // 2 + 1
@@ -84,8 +81,8 @@ for q in range(Q):
         Psi_i = micarray.modal.angular.cht_matrix(max_cht_order, phi_i, 1/M)
         H_cht += np.matmul(H_i, np.conj(Psi_i.T))
     Psi = micarray.modal.angular.cht_matrix(max_cht_order, azi)
-    H = np.matmul(H_cht, Psi).T
-    h[q, :, :] = np.fft.irfft(H, axis=-1)
+    H_q = np.matmul(H_cht, Psi).T
+    h[q, :, :] = np.fft.irfft(H_q, axis=-1)
 hrs = np.reshape(h, (Q*M, N))
 
 # Original impulse responses
@@ -101,8 +98,29 @@ for i, theta in enumerate(elev):
     h0[i, :, :] = htemp
 h0rs = np.reshape(h0, (Q*M, N))
     
-# Evaluation
-system_distance = nmse(hrs, h0rs, normalize='each')
+# Evaluating individual impulse responses
+system_distance = nmse(hrs, h0rs, normalize='mean')
+
+# Monofrequency analysis
+f = 2000
+H = np.matmul(hrs, np.exp(1j*2*np.pi*f/fs*np.arange(N)))
+H0 = np.matmul(h0rs, np.exp(1j*2*np.pi*f/fs*np.arange(N)))
+azi, elev, weights = micarray.modal.angular.grid_gauss(max_sht_order)
+Yp = micarray.modal.angular.sht_matrix(max_sht_order, azi, elev, weights)
+Hnm = np.matmul(np.conj(Yp), H)
+H0nm = np.matmul(np.conj(Yp), H0)
+
+# reconstruction (synthesis) on the sphere surfrace
+K = 50
+azi = np.linspace(0, 2*np.pi, num=K)
+elev = np.linspace(np.pi/K, np.pi, num=K)
+Azi, Elev = np.meshgrid(azi, elev)
+Y = micarray.modal.angular.sht_matrix(max_sht_order, np.ndarray.flatten(Azi), np.ndarray.flatten(Elev))
+S = np.squeeze(np.matmul(Y.T, Hnm[:, np.newaxis]))
+S = np.reshape(S, (K, K))
+S0 = np.squeeze(np.matmul(Y.T, H0nm[:, np.newaxis]))
+S0 = np.reshape(S0, (K, K))
+
 
 # Plots
 nn = np.random.randint(0, Q*M)
@@ -113,6 +131,45 @@ plt.plot(db(h0rs[nn, :]))
 plt.figure()
 plt.plot(db(system_distance))
 
+
 plt.figure()
-plt.pcolormesh(db(hrs-h0rs))
+plt.pcolormesh(db(h0rs))
 plt.colorbar()
+
+plt.figure()
+plt.pcolormesh(db(hrs))
+plt.colorbar()
+
+
+plt.figure()
+plt.plot(db(H))
+plt.plot(db(H0))
+
+
+plt.figure()
+plt.pcolormesh(np.real(S))
+plt.colorbar()
+
+plt.figure()
+plt.pcolormesh(np.real(S0))
+plt.colorbar()
+
+plt.figure()
+plt.pcolormesh(np.real(S-S0))
+plt.colorbar()
+
+
+
+plt.figure()
+plt.pcolormesh(db(S))
+plt.colorbar(label='dB')
+
+plt.figure()
+plt.pcolormesh(db(S0))
+plt.colorbar(label='dB')
+
+plt.figure()
+plt.pcolormesh(db(S-S0))
+plt.colorbar(label='dB')
+
+
